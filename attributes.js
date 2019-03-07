7c3cf3a8371fec5faf971baf06903ee931852f38
Christos Fotos <c.foutsikas@gmail.com>
2019-03-07 19:26:30 +0200

    Change CSS values

--- attributes.js
+++ attributes.js
@@ -61,9 +61,9 @@
                 el.html(
                     `<div class="attribute-container">\
                             <div class="attribute">\
-                                <div class="button" onclick="$(this).parent().parent().parent().decrementValue();"><span>-</span></div>
-                                <div class="value"><span>${value}</span></div>
-                                <div class="button" onclick="$(this).parent().parent().parent().incrementValue();"><span>+</span></div>
+                                <div class="button" onclick="$(this).parent().parent().parent().decrementValue();"><span>-</span></div>\
+                                <div class="value"><span>${value}</span></div>\
+                                <div class="button" onclick="$(this).parent().parent().parent().incrementValue();"><span>+</span></div>\
                             </div>\
                             <div class="modifier" onclick="$(this).parent().parent().DiceRoll();">\
                                 <span>${modifier}</span>\
